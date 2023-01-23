ced44f783161e3e6eee4dfb98f58f8a159332dae
oereo <dlstpgns0406@gmail.com>
2023-01-23 20:48:37 +0900

    chore: project setting

---
+++ src/main/java/com/twentiethcenturygangsta/JamBoard/JamBoardApplication.java
@@ -0,0 +1,13 @@
+package com.twentiethcenturygangsta.JamBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamBoardApplication.class, args);
+	}
+
+}
