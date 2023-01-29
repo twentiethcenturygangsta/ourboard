a5b0e75934a49875233540291b72acbe150d57a8
oereo <dlstpgns0406@gmail.com>
2023-01-29 15:58:41 +0900

    feat: user가 설정한 group 별로 entity name과 description 객체를 담은 hashMap 반환 로직 추가

--- src/main/java/com/twentiethcenturygangsta/jamboard/services/TableService.java
+++ src/main/java/com/twentiethcenturygangsta/jamboard/services/TableService.java
@@ -1,18 +1,24 @@
 package com.twentiethcenturygangsta.jamboard.services;
 
 import com.twentiethcenturygangsta.jamboard.dto.Table;
+import com.twentiethcenturygangsta.jamboard.dto.TablesInfo;
 import com.twentiethcenturygangsta.jamboard.repository.ListRepository;
 import com.twentiethcenturygangsta.jamboard.site.JamBoardClient;
+import com.twentiethcenturygangsta.jamboard.trace.JamBoardEntity;
 import com.twentiethcenturygangsta.jamboard.trace.Trace;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.lang.annotation.Annotation;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class TableService {
     private final JamBoardClient jamBoardClient;
@@ -20,15 +26,43 @@ public class TableService {
 
     @Trace
     public Table getTableData(String tableName) throws SQLException {
+        log.info("connection ={} ", jamBoardClient.getConnection());
         return listRepository.findAll(tableName, jamBoardClient.getConnection());
     }
 
-    public List<String> getTableSimpleNames() {
+    public HashMap<String, ArrayList<TablesInfo>> getTableSimpleNames() {
         List<String> tableSimpleNames = new ArrayList<>();
+        HashMap<String, ArrayList<TablesInfo>> dict = new HashMap<>();
         for (Class table : jamBoardClient.getTables()) {
             tableSimpleNames.add(table.getSimpleName());
+
+            log.info("annotation = {}", table.getAnnotation(JamBoardEntity.class));
+
+            for (Annotation annotation : table.getAnnotations()) {
+                if (annotation instanceof JamBoardEntity myAnnotation) {
+                    if (dict.containsKey(myAnnotation.group())) {
+                        dict.get(myAnnotation.group()).add(
+                                TablesInfo.builder()
+                                        .tableName(table.getSimpleName())
+                                        .description(myAnnotation.description())
+                                        .build()
+                        );
+                    } else {
+                        dict.put(myAnnotation.group(), new ArrayList<>());
+                        dict.get(myAnnotation.group()).add(
+                                TablesInfo.builder()
+                                        .tableName(table.getSimpleName())
+                                        .description(myAnnotation.description())
+                                        .build()
+                        );
+                    }
+                    log.info("name: {}" + myAnnotation.group());
+                    log.info("value: {}" + myAnnotation.description());
+                }
+            }
         }
-        return tableSimpleNames;
+        log.info("result = {}", dict);
+        return dict;
     }
 
 }
