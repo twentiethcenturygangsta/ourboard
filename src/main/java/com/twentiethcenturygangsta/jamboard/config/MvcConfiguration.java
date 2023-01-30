428e15b749294c4ef143e4e17152b3a009f640fc
oereo <dlstpgns0406@gmail.com>
2023-01-30 14:46:43 +0900

    feat: MvcConfiguration class 추가
    - static file config

---
+++ src/main/java/com/twentiethcenturygangsta/jamboard/config/MvcConfiguration.java
@@ -0,0 +1,14 @@
+package com.twentiethcenturygangsta.jamboard.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+    }
+}
