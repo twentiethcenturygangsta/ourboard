1d0aac196a7b28866e36f13cdcfe9e1f0c21a96b
oereo <dlstpgns0406@gmail.com>
2023-02-07 23:03:04 +0900

    feat: LoginForm dto class 추가

---
+++ src/main/java/com/twentiethcenturygangsta/ourboard/form/LoginForm.java
@@ -0,0 +1,12 @@
+package com.twentiethcenturygangsta.ourboard.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginForm {
+
+    private String memberId;
+    private String password;
+}
