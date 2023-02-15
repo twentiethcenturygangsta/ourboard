293e3f6766b88519e8f03269d0c0661a886a62e9
oereo <dlstpgns0406@gmail.com>
2023-02-15 23:36:23 +0900

    feat: applicationContext 의존성 주입

--- src/main/java/com/twentiethcenturygangsta/ourboard/config/ShardConfigurationReference.java
+++ src/main/java/com/twentiethcenturygangsta/ourboard/config/ShardConfigurationReference.java
@@ -1,9 +1,14 @@
 package com.twentiethcenturygangsta.ourboard.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.twentiethcenturygangsta.ourboard")
+@RequiredArgsConstructor
 public class ShardConfigurationReference {
+    private final ApplicationContext appContext;
+
 }
