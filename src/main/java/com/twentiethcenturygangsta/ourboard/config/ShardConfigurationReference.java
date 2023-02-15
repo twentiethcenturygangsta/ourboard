306a8c4b2c709d1341f90edf29eaf69fb1553ada
oereo <dlstpgns0406@gmail.com>
2023-02-15 23:41:58 +0900

    feat: registerBasePackage bean 추가

--- src/main/java/com/twentiethcenturygangsta/ourboard/config/ShardConfigurationReference.java
+++ src/main/java/com/twentiethcenturygangsta/ourboard/config/ShardConfigurationReference.java
@@ -1,9 +1,13 @@
 package com.twentiethcenturygangsta.ourboard.config;
 
+import com.twentiethcenturygangsta.ourboard.annoatation.EnableOurBoard;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 @ComponentScan("com.twentiethcenturygangsta.ourboard")
@@ -11,4 +15,9 @@ import org.springframework.context.annotation.Configuration;
 public class ShardConfigurationReference {
     private final ApplicationContext appContext;
 
+    @Bean
+    public void registerBasePackage() {
+        Map<String, Object> beans = appContext.getBeansWithAnnotation(EnableOurBoard.class);
+        String basePackage = beans.getClass().getPackageName();
+    }
 }
