386b0c56f8503804ffa94a065c3d52f36e9b5c99
oereo <dlstpgns0406@gmail.com>
2023-02-16 17:38:32 +0900

    feat: registerBasePackage method 추가

--- src/main/java/com/twentiethcenturygangsta/ourboard/config/ShardConfigurationReference.java
+++ src/main/java/com/twentiethcenturygangsta/ourboard/config/ShardConfigurationReference.java
@@ -2,6 +2,7 @@ package com.twentiethcenturygangsta.ourboard.config;
 
 import com.twentiethcenturygangsta.ourboard.annoatation.EnableOurBoard;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
 
+
+@Slf4j
 @Configuration
 @ComponentScan("com.twentiethcenturygangsta.ourboard")
 @RequiredArgsConstructor
@@ -16,8 +19,12 @@ public class ShardConfigurationReference {
     private final ApplicationContext appContext;
 
     @Bean
-    public void registerBasePackage() {
+    public String registerBasePackage() {
+        String basePackage = "";
         Map<String, Object> beans = appContext.getBeansWithAnnotation(EnableOurBoard.class);
-        String basePackage = beans.getClass().getPackageName();
+        for(String key : beans.keySet()) {
+            basePackage = beans.get(key).getClass().getPackageName();
+        }
+        return basePackage;
     }
 }
