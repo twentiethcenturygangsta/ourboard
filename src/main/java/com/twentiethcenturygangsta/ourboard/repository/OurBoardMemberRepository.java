649f937c1578b5fa3fb7f3eaa8ef8a9ad05426e3
oereo <dlstpgns0406@gmail.com>
2023-02-17 23:21:25 +0900

    feat: OurBoardMemberRepository에 pagination findAll method 추가

--- src/main/java/com/twentiethcenturygangsta/ourboard/repository/OurBoardMemberRepository.java
+++ src/main/java/com/twentiethcenturygangsta/ourboard/repository/OurBoardMemberRepository.java
@@ -1,6 +1,8 @@
 package com.twentiethcenturygangsta.ourboard.repository;
 
 import com.twentiethcenturygangsta.ourboard.entity.OurBoardMember;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,5 @@ public interface OurBoardMemberRepository extends JpaRepository<OurBoardMember, 
 
     Optional<OurBoardMember> findOurBoardMemberByMemberId(String memberId);
     boolean existsOurBoardMemberByMemberIdAndPassword(String memberId, String password);
+    Page<OurBoardMember> findAll(Pageable pageable);
 }
