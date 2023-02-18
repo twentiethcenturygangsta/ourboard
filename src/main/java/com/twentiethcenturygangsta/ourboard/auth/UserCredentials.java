c5401fa331f9f336ff0d91d7062fc58391023114
junhyeok <wnsdp01@naver.com>
2023-02-18 14:20:14 +0900

    feat : password 암호화, 복호화 클래스 추가 및 유저 Default Role 입력

--- src/main/java/com/twentiethcenturygangsta/ourboard/auth/UserCredentials.java
+++ src/main/java/com/twentiethcenturygangsta/ourboard/auth/UserCredentials.java
@@ -9,18 +9,15 @@ public class UserCredentials {
     private final String password;
     private final Role role;
 
-    public UserCredentials(String userName, String password, Role role) {
+    public UserCredentials(String userName, String password) {
         if (userName == null) {
             throw new IllegalArgumentException("Username cannot be null.");
         }
         if (password == null) {
             throw new IllegalArgumentException("Password cannot be null");
         }
-        if (role == null) {
-            throw new IllegalArgumentException("role cannot be null");
-        }
         this.userName = userName;
         this.password = password;
-        this.role = role;
+        this.role = Role.SUPER_USER;
     }
 }
