479a44a4bc3c1d75d0ce512bb35dff5938225e48
oereo <dlstpgns0406@gmail.com>
2023-02-19 22:16:25 +0900

    feat: Error dto class 추가

---
+++ src/main/java/com/twentiethcenturygangsta/ourboard/exception/Error.java
@@ -0,0 +1,21 @@
+package com.twentiethcenturygangsta.ourboard.exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+public class Error {
+    private String timestamp;
+    private String code;
+    private String field;
+    private String message;
+
+    @Builder
+    public Error(String timestamp, String code, String field, String message) {
+        this.timestamp = timestamp;
+        this.code = code;
+        this.field = field;
+        this.message = message;
+    }
+}
