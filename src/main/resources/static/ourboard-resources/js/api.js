a89a43ee8322f194089d3f3c2a8c8902e50a4d27
oereo <dlstpgns0406@gmail.com>
2023-02-21 17:18:32 +0900

    refactor: input data check function 분리

--- src/main/resources/static/ourboard-resources/js/api.js
+++ src/main/resources/static/ourboard-resources/js/api.js
@@ -2,3 +2,13 @@ const APICall = axios.create({
     timeout: 1000,
     headers: {'Content-Type': "application/json"}
 })
+
+const checkInputForm = (arr) => {
+    arr.filter((instance) => {
+        if (instance.val() === "") {
+            instance.addClass("errorInput")
+        } else {
+            instance.removeClass("errorInput")
+        }
+    })
+}
