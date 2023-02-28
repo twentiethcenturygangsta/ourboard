f43bcab53c973814fe9edf046a0b69d486b31c2b
oereo <dlstpgns0406@gmail.com>
2023-02-28 14:45:05 +0900

    feat: add createInstanceAPI

--- src/main/java/com/twentiethcenturygangsta/ourboard/controller/AdminAPIController.java
+++ src/main/java/com/twentiethcenturygangsta/ourboard/controller/AdminAPIController.java
@@ -5,6 +5,7 @@ import com.twentiethcenturygangsta.ourboard.form.LoginForm;
 import com.twentiethcenturygangsta.ourboard.handler.ResponseHandler;
 import com.twentiethcenturygangsta.ourboard.manager.session.SessionConst;
 import com.twentiethcenturygangsta.ourboard.services.LoginService;
+import com.twentiethcenturygangsta.ourboard.services.TableService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -13,9 +14,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
 
 
 @Slf4j
@@ -25,6 +29,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminAPIController {
 
     private final LoginService loginService;
+    private final TableService tableService;
 
     @PostMapping("/login")
     public ResponseEntity<Object> loginAPI(@RequestBody LoginForm loginForm, HttpServletRequest request) throws Exception {
@@ -35,5 +40,14 @@ public class AdminAPIController {
         HttpSession session = request.getSession();
         session.setAttribute(SessionConst.LOGIN_MEMBER, ourBoardMember);
         return ResponseHandler.generateResponse(HttpStatus.OK, "Successful login");
+    }
+
+    @PostMapping("/{tableName}")
+    public ResponseEntity<Object> createInstanceAPI(@PathVariable("tableName") String tableName,
+                                                    @RequestBody HashMap<String, Object> requestBody) throws Exception {
+        log.info("requestBody = {}", requestBody);
+        log.info("tableName = {}", tableName);
+        tableService.createObject(requestBody, tableName);
+        return ResponseHandler.generateResponse(HttpStatus.OK, "Successful create");
     }
 }
