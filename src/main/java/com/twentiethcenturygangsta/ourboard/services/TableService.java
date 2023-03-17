e47627dbfe910a36c60b8151af9a23fff3e89192
oereo <dlstpgns0406@gmail.com>
2023-03-17 22:54:29 +0900

    refactor: jitpack parameter

--- src/main/java/com/twentiethcenturygangsta/ourboard/services/TableService.java
+++ src/main/java/com/twentiethcenturygangsta/ourboard/services/TableService.java
@@ -105,8 +105,7 @@ public class TableService {
         for (Map.Entry<String, Entity> entity : entities.entrySet()) {
 
             for (Annotation annotation : entity.getValue().getEntityClass().getAnnotations()) {
-                if (annotation instanceof OurBoardEntity) {
-                    OurBoardEntity myAnnotation = (OurBoardEntity) annotation;
+                if (annotation instanceof OurBoardEntity myAnnotation) {
                     if (dict.containsKey(myAnnotation.group())) {
                         dict.get(myAnnotation.group()).add(
                                 TablesInfo.builder()
